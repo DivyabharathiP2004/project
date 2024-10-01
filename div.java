a0501a13d281f0e824df2a8736f0b8ed652e0640
DivyabharathiP2004 <mpugazhendi25@gmail.com>
2024-10-01 16:25:14 +0530

    Add files via upload

---
+++ div.java
@@ -0,0 +1,19 @@
+public interface I1 {
+    public static final int model=2020;
+    public static final brand="tata"
+    void method 1();
+    static void m2()
+    {
+        System.out.println("inside I1");
+    }
+}
+public interface I2{
+    public static final string availabilty="yes"
+    void method2();
+    {
+        public interface IS extends I1I2{
+            public static final inti=0;
+            void method();
+        }
+    }
+}
